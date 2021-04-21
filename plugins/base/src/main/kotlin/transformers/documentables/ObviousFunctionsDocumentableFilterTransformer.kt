14aacb6a02e66d3097f2f9f514ff8e7ca6ba14b5
Roy Hopkins <roy.hopkins@r3.com>
2021-04-21 09:43:57 +0100

    Suppress obvious properties if suppressObviousFunctions is set.

--- plugins/base/src/main/kotlin/transformers/documentables/ObviousFunctionsDocumentableFilterTransformer.kt
+++ plugins/base/src/main/kotlin/transformers/documentables/ObviousFunctionsDocumentableFilterTransformer.kt
@@ -1,11 +1,22 @@
 package org.jetbrains.dokka.base.transformers.documentables
 
-import org.jetbrains.dokka.model.DFunction
-import org.jetbrains.dokka.model.Documentable
-import org.jetbrains.dokka.model.ObviousMember
+import org.jetbrains.dokka.model.*
 import org.jetbrains.dokka.plugability.DokkaContext
 
 class ObviousFunctionsDocumentableFilterTransformer(context: DokkaContext) : SuppressedByConditionDocumentableFilterTransformer(context) {
-    override fun shouldBeSuppressed(d: Documentable): Boolean =
-        context.configuration.suppressObviousFunctions && d is DFunction && d.extra[ObviousMember] != null
+    override fun shouldBeSuppressed(d: Documentable): Boolean {
+        return when (d) {
+            is DFunction -> {
+                context.configuration.suppressObviousFunctions && d.extra[ObviousMember] != null
+            }
+            is DProperty -> {
+                val inherited = d.extra[InheritedMember]?.inheritedFrom?.map { "${it.value?.packageName.orEmpty()}.${it.value?.classNames.orEmpty()}" }
+                context.configuration.suppressObviousFunctions &&
+                        inherited?.any { it == "kotlin.Any" || it == "kotlin.Enum" || it == "kotlin.Throwable" } ?: false
+            }
+            else -> {
+                false
+            }
+        }
+    }
 }
